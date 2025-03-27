1a4f280a7a63f26623e4341c956e414d471cac4a
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-03-27 22:07:06 +0530

    enhance css boooking searchbar✅🌟

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <h1 className="sm:text-6xl font-bold text-[#ffce46] text-center text-3xl p-5">
           Discover Comfort & Luxury <br /> Book Your Stay Today!
         </h1>
-        <div className="text-white font-bolds flex gap-x-9 text-2xl items-center  p-5 flex-col sm:flex-row sm:transform sm:skew-x-12 rounded-bl-2xl rounded-tr-2xl border-2">
+        <div className="text-white font-bolds flex gap-y-10  sm:gap-x-10 text-2xl items-center  p-5 flex-col sm:flex-row sm:transform  rounded-bl-2xl rounded-tr-2xl border-2">
           <div className="flex flex-col">
             <label htmlFor="">Check In</label>
             <input type="date" className="border border-[#FFD700] b- w-64" />
@@ -14,15 +14,23 @@ export default function Header() {
             <label htmlFor="">Check In</label>
             <input type="date" className="border border-[#FFD700] w-64" />
           </div>
-          <div className="flex flex-col">
-            <label htmlFor="">Adult</label>
-            <input type="text" className="border border-[#FFD700] w-64" />
+          <div>
+            <select name="category" id="category">
+              <option value="" className="text-black">
+                Select Category
+              </option>
+              <option value="Luxury" className="text-black">
+                Luxury
+              </option>
+              <option value="Normal" className="text-black">
+                Normal
+              </option>
+              <option value="Dulux" className="text-black">
+                Dulux
+              </option>
+            </select>
           </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="">Child</label>
-            <input type="text" className="border border-[#FFD700] w-64" />
-          </div>
           <div>
             <button className="bg-amber-300 p-4 rounded-[130px] ">
               Book Now

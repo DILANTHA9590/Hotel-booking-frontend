fa31a43fe87296c5795b83f1f94dbe988a938bdb
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-03-28 23:07:57 +0530

    Import category component into homepage📄✅

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import Navigationbar from "../component/navigationbar";
 import Header from "../component/header";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import Categories from "../component/categories";
 
 export default function HomePage() {
   const images = [
@@ -42,92 +43,8 @@ export default function HomePage() {
         </div>
       </div>
 
-      <section className="relative bg-pri">
-        {/* Background div with lower z-index */}
-        <div className="bg-gray-800 h-[30vh] absolute w-full bottom-0 z-10">
-          oooss
-        </div>
-
-        {/* Cards Container - Higher z-index */}
-        <div className="relativ sm:h-[60vh] font-bold pt-4 z-50">
-          <h1 className="text-4xl text-center text-title">
-            <span className="text-secondry">Explore</span> Stays by Category
-          </h1>
-
-          <div className="flex sm:w-[60%] w-full mx-auto gap-x-4 mt-6 flex-col sm:flex-row gap-y-4 relative">
-            {/* Card 1 */}
-            <div className=" bg-white relative z-50">
-              <img
-                className="p-4"
-                src="https://th.bing.com/th/id/OIP.yybHXZkwrX_eXpNERNrMzAHaE7?rs=1&pid=ImgDetMain"
-                alt=""
-              />
-              <h2 className="text-secondry text-2xl text-center">
-                Standard Room
-              </h2>
-              <div className="flex justify-between">
-                <div>
-                  <div className="flex justify-center items-center bg-secondry p-4 rounded-4xl h-[40px] ml-4">
-                    <Link>Explore more</Link>
-                    <FaLongArrowAltRight />
-                  </div>
-                </div>
-                <div className="pr-4">
-                  <h2>$60.00</h2>
-                  <h2>per night</h2>
-                </div>
-              </div>
-            </div>
-
-            {/* Card 2 */}
-            <div className=" bg-white relative z-50">
-              <img
-                className="p-4"
-                src="https://th.bing.com/th/id/OIP.yybHXZkwrX_eXpNERNrMzAHaE7?rs=1&pid=ImgDetMain"
-                alt=""
-              />
-              <h2 className="text-secondry text-2xl text-center">
-                Deluxe Room
-              </h2>
-              <div className="flex justify-between">
-                <div>
-                  <div className="flex justify-center items-center bg-secondry p-4 rounded-4xl h-[40px] ml-4">
-                    <Link>Explore more</Link>
-                    <FaLongArrowAltRight />
-                  </div>
-                </div>
-                <div className="pr-4">
-                  <h2>$60.00</h2>
-                  <h2>per night</h2>
-                </div>
-              </div>
-            </div>
-
-            {/* Card 3 */}
-            <div className=" bg-white relative z-50">
-              <img
-                className="p-4"
-                src="https://th.bing.com/th/id/OIP.yybHXZkwrX_eXpNERNrMzAHaE7?rs=1&pid=ImgDetMain"
-                alt=""
-              />
-              <h2 className="text-secondry text-2xl text-center">
-                Luxury Room
-              </h2>
-              <div className="flex justify-between">
-                <div>
-                  <div className="flex justify-center items-center bg-secondry p-4 rounded-4xl max-h-[40px] ml-4">
-                    <Link>Explore more</Link>
-                    <FaLongArrowAltRight />
-                  </div>
-                </div>
-                <div className="pr-4">
-                  <h2>$60.00</h2>
-                  <h2>per night</h2>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <section>
+        <Categories />
       </section>
     </>
   );

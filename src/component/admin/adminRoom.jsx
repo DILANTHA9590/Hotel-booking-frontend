6a14c0c45f14abbd454cea7fe14535d3ad5b39d1
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-03-30 10:11:55 +0530

    Add loading screen while fetching room data✅🌟

--- src/component/admin/adminRoom.jsx
+++ src/component/admin/adminRoom.jsx
@@ -12,6 +12,7 @@ export default function AdminRooms() {
   const [rooms, setRooms] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [loaded, setloaded] = useState(false);
 
   const navigate = useNavigate();
 
@@ -22,30 +23,32 @@ export default function AdminRooms() {
       navigate("/login");
       return;
     }
-
-    axios
-      .post(
-        `${import.meta.env.VITE_BACKEND_URL}/api/rooms/getall`,
-        {
-          page: page,
-          pageSize: 10,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
+    if (!loaded) {
+      axios
+        .post(
+          `${import.meta.env.VITE_BACKEND_URL}/api/rooms/getall`,
+          {
+            page: page,
+            pageSize: 10,
           },
-        }
-      )
-      .then((res) => {
-        console.log(res);
-        setRooms(res.data.rooms);
-        setTotalPages(res.data.pagination.totalPages);
-      })
-      .catch((err) => {
-        console.log(err);
-        toast.error("Something went a wrong please try again");
-      });
-  }, [page]);
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          setRooms(res.data.rooms);
+          setTotalPages(res.data.pagination.totalPages);
+          setloaded(true);
+        })
+        .catch((err) => {
+          console.log(err);
+          toast.error("Something went a wrong please try again");
+        });
+    }
+  }, [page, loaded]);
 
   function handleOnDelete(roomid) {
     const token = localStorage.getItem("token");
@@ -66,107 +69,120 @@ export default function AdminRooms() {
   return (
     <>
       <div className="container mx-auto bg-gray-500 p-4 h-screen  overflow-hidden overflow-y-scroll relative ">
-        <div className="absolute top-0 left-0 w-full h-full z-50 flex justify-center items-center">
-          <p className="w-[100px] h-[100px] border border-primary border-t-4 border-t-blue-700 rounded-full animate-spin"></p>
-        </div>
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-gray-200s shadow-md rounded-lg">
-            <thead>
-              <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal border">
-                <th className="py-3 px-6 text-left border">Room id</th>
-                <th className="py-3 px-6 text-left">Category</th>
-                <th className="py-3 px-6 text-left border">Maxguests</th>
-                <th className="py-3 px-6 text-left border">Availbale</th>
-                <th className="py-3 px-6 text-left border">Photos</th>
-                <th className="py-3 px-6 text-left border">
-                  Special description
-                </th>
-                <th className="py-3 px-6 text-left border">Note</th>
+        {loaded ? (
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-gray-200s shadow-md rounded-lg">
+              <thead>
+                <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal border">
+                  <th className="py-3 px-6 text-left border">Room id</th>
+                  <th className="py-3 px-6 text-left">Category</th>
+                  <th className="py-3 px-6 text-left border">Maxguests</th>
+                  <th className="py-3 px-6 text-left border">Availbale</th>
+                  <th className="py-3 px-6 text-left border">Photos</th>
+                  <th className="py-3 px-6 text-left border">
+                    Special description
+                  </th>
+                  <th className="py-3 px-6 text-left border">Note</th>
 
-                <th className="py-3 px-6 text-left border">Delete</th>
+                  <th className="py-3 px-6 text-left border">Delete</th>
 
-                <th className="py-3 px-6 text-left border">Update</th>
-              </tr>
-            </thead>
-            <tbody className="text-gray-600 text-sm font-light">
-              {rooms.map((room, index) => (
-                <tr
-                  key={room.roomId}
-                  className="border-b border-gray-200 hover:bg-gray-100"
-                >
-                  <td className="py-3 px-6 text-left border">{room.roomId}</td>
-                  <td className="py-3 px-6 text-left border">
-                    {room.category}
-                  </td>
-
-                  <td className="py-3 px-6 text-left border">
-                    {room.maxGuests}
-                  </td>
-
-                  <td className="py-3 px-6 text-left border">
-                    {room.available}
-                  </td>
-                  <td className="py-3 px-6 text-left border">
-                    {room.photos[0]}
-                  </td>
-                  <td className="py-3 px-6 text-left border">
-                    {room.specialsDescriptions}
-                  </td>
-                  <td className="py-3 px-6 text-left border">{room.notes}</td>
-                  <td className="py-3 px-6 text-left border">
-                    <button
-                      className="py-3 px-6"
-                      onClick={() => handleOnDelete(room.roomId)}
-                    >
-                      <MdDelete />
-                    </button>
-                  </td>
-                  <td className="py-3 px-6 text-left border">
-                    <button
-                      className="py-3 px-6 "
-                      onClick={() => {
-                        navigate("", {
-                          state: {},
-                        });
-                      }}
-                    >
-                      <AiFillEdit />
-                    </button>
-                  </td>
+                  <th className="py-3 px-6 text-left border">Update</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className="flex gap-x-5 justify-center p-3">
-            <button
-              onClick={() => setPage((prevPage) => Math.max(prevPage - 1, 1))}
-            >
-              <MdArrowBackIosNew />
-            </button>
-            {Array.from({ length: totalPages }).map((item, index) => {
-              return (
-                <div>
-                  <button
-                    onClick={() => setPage(index + 1)}
-                    key={index}
-                    className={`p-6 bg-blue-400 w-[40px] h-[40px] flex items-center justify-center text 
-                    ${page == index + 1 && `border-2 border-black`}`}
+              </thead>
+              <tbody className="text-gray-600 text-sm font-light">
+                {rooms.map((room, index) => (
+                  <tr
+                    key={room.roomId}
+                    className="border-b border-gray-200 hover:bg-gray-100"
                   >
-                    {index + 1}
-                  </button>
-                </div>
-              );
-            })}
+                    <td className="py-3 px-6 text-left border">
+                      {room.roomId}
+                    </td>
+                    <td className="py-3 px-6 text-left border">
+                      {room.category}
+                    </td>
 
-            <button
-              onClick={() =>
-                setPage((prevPage) => Math.min(prevPage + 1, totalPages))
-              }
-            >
-              <IoIosArrowForward />
-            </button>
+                    <td className="py-3 px-6 text-left border">
+                      {room.maxGuests}
+                    </td>
+
+                    <td className="py-3 px-6 text-left border">
+                      {room.available}
+                    </td>
+                    <td className="py-3 px-6 text-left border">
+                      {room.photos[0]}
+                    </td>
+                    <td className="py-3 px-6 text-left border">
+                      {room.specialsDescriptions}
+                    </td>
+                    <td className="py-3 px-6 text-left border">{room.notes}</td>
+                    <td className="py-3 px-6 text-left border">
+                      <button
+                        className="py-3 px-6"
+                        onClick={() => handleOnDelete(room.roomId)}
+                      >
+                        <MdDelete />
+                      </button>
+                    </td>
+                    <td className="py-3 px-6 text-left border">
+                      <button
+                        className="py-3 px-6 "
+                        onClick={() => {
+                          navigate("", {
+                            state: {},
+                          });
+                        }}
+                      >
+                        <AiFillEdit />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className="flex gap-x-5 justify-center p-3">
+              <button
+                onClick={() =>
+                  setPage(
+                    (prevPage) => Math.max(prevPage - 1, 1),
+                    setloaded(false)
+                  )
+                }
+              >
+                <MdArrowBackIosNew />
+              </button>
+              {Array.from({ length: totalPages }).map((item, index) => {
+                return (
+                  <div>
+                    <button
+                      onClick={() => setPage(index + 1) || setloaded(false)}
+                      key={index}
+                      className={`p-6 bg-blue-400 w-[40px] h-[40px] flex items-center justify-center text 
+                  ${page == index + 1 && `border-2 border-black`}`}
+                    >
+                      {index + 1}
+                    </button>
+                  </div>
+                );
+              })}
+
+              <button
+                onClick={() =>
+                  setPage(
+                    (prevPage) => Math.min(prevPage + 1, totalPages),
+                    setloaded(false)
+                  )
+                }
+              >
+                <IoIosArrowForward />
+              </button>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="absolute top-0 left-0 w-full h-full z-50 flex justify-center items-center ">
+            <p className="w-[100px] h-[100px] border border-primary border-t-4 border-t-blue-700 rounded-full animate-spin"></p>
+          </div>
+        )}
       </div>
     </>
   );

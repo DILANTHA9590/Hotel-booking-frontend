e7364daffd041e29e978d8602d1b5cec71f1c0ef
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-04-02 07:00:35 +0530

    create standard room component✅🌟

---
+++ src/component/standardRoom.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+export default function StandardRoom() {
+  useEffect(() => {}, []);
+  return (
+    <>
+      <div className="bg-primary h-full"></div>
+    </>
+  );
+}

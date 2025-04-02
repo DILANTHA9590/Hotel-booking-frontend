2972713bf95538486168adfe89649affc9a1c220
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-04-02 12:42:21 +0530

    Removed unused useState variables and react icon links✅🌟

--- src/component/admin/adminGalleryItem.jsx
+++ src/component/admin/adminGalleryItem.jsx
@@ -4,11 +4,10 @@ import axios from "axios";
 import { MdDelete } from "react-icons/md";
 import { AiFillEdit } from "react-icons/ai";
 import { IoMdAdd } from "react-icons/io";
-
+import { IoIosAdd } from "react-icons/io";
 export default function AdminGalleryItem() {
   const navigate = useNavigate();
   const [galleryItems, setGalleryItem] = useState([]);
-
   const [loaded, setloaded] = useState(false);
 
   useEffect(() => {
@@ -67,73 +66,80 @@ export default function AdminGalleryItem() {
     <>
       <div className="container mx-auto bg-white p-4 h-screen  overflow-hidden overflow-y-scroll relative ">
         {/* <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-50 flex justify-center items-center"> </div> */}
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-gray-200s shadow-md rounded-lg">
-            <thead>
-              <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal border">
-                <th className="py-3 px-6 text-left border">Count</th>
-                <th className="py-3 px-6 text-left border">Name</th>
-                <th className="py-3 px-6 text-left border">Image</th>
-                <th className="py-3 px-6 text-left border">description</th>
+        {loaded ? (
+          <div>
+            <button
+              className="flex items-center justify-centr font-bold bg-green-600 p-3 mb-1"
+              onClick={() => navigate("/admin/addgallery")}
+            >
+              Add Gallery
+              <IoIosAdd className="text-2xl ml-3 font-bold transition-transform duration-500 hover:rotate-[360deg]" />
+            </button>
+            <div className="overflow-x-auto">
+              <table className="min-w-full bg-gray-200s shadow-md rounded-lg">
+                <thead>
+                  <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal border">
+                    <th className="py-3 px-6 text-left border">Count</th>
+                    <th className="py-3 px-6 text-left border">Name</th>
+                    <th className="py-3 px-6 text-left border">Image</th>
+                    <th className="py-3 px-6 text-left border">description</th>
 
-                <th className="py-3 px-6 text-left border">add Galley</th>
-                <th className="py-3 px-6 text-left border">Edit</th>
-                <th className="py-3 px-6 text-left border">delete</th>
-              </tr>
-            </thead>
-            <tbody className="text-gray-600 text-sm font-light">
-              {galleryItems.map((galleryItem, index) => (
-                <tr
-                  key={galleryItem._id}
-                  className="border-b border-gray-200 hover:bg-gray-100"
-                >
-                  <td className="py-3 px-6  border text-center">{number++}</td>
-                  <td className="py-3 px-6 text-left border">
-                    {galleryItem.name}
-                  </td>
-
-                  <td className="py-3 px-6 text-left border max-w-[200px] truncate overflow-hidden">
-                    {galleryItem.image}
-                  </td>
-                  <td className="py-3 px-6 text-left border">
-                    {galleryItem.description}
-                  </td>
-
-                  <td className="py-3 px-6  border text-center">
-                    <button
-                      className="py-3 px-6"
-                      onClick={() => navigate("/admin/addgallery")}
+                    <th className="py-3 px-6 text-left border">Edit</th>
+                    <th className="py-3 px-6 text-left border">delete</th>
+                  </tr>
+                </thead>
+                <tbody className="text-gray-600 text-sm font-light">
+                  {galleryItems.map((galleryItem, index) => (
+                    <tr
+                      key={galleryItem._id}
+                      className="border-b border-gray-200 hover:bg-gray-100"
                     >
-                      <IoMdAdd className="w-[25px] h-[25px] text-green-400 cursor-pointer" />
-                    </button>
-                  </td>
+                      <td className="py-3 px-6  border text-center">
+                        {number++}
+                      </td>
+                      <td className="py-3 px-6 text-left border">
+                        {galleryItem.name}
+                      </td>
 
-                  <td className="py-3 px-6 text-left border">
-                    <button
-                      className="py-3 px-6 "
-                      onClick={() => {
-                        navigate("/admin/editgallery", {
-                          state: { galleryItem: galleryItem },
-                        });
-                      }}
-                    >
-                      <AiFillEdit className="w-[25px] h-[25px] text-yellow-400 cursor-pointer" />
-                    </button>
-                  </td>
+                      <td className="py-3 px-6 text-left border max-w-[200px] truncate overflow-hidden">
+                        {galleryItem.image}
+                      </td>
+                      <td className="py-3 px-6 text-left border">
+                        {galleryItem.description}
+                      </td>
 
-                  <td className="py-3 px-6 text-left border">
-                    <button
-                      className="py-3 px-6"
-                      onClick={() => handleOnDelete(galleryItem._id)}
-                    >
-                      <MdDelete className="w-[25px] h-[25px] text-red-400 cursor-pointer" />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                      <td className="py-3 px-6 text-left border">
+                        <button
+                          className="py-3 px-6 "
+                          onClick={() => {
+                            navigate("/admin/editgallery", {
+                              state: { galleryItem: galleryItem },
+                            });
+                          }}
+                        >
+                          <AiFillEdit className="w-[25px] h-[25px] text-yellow-400 cursor-pointer" />
+                        </button>
+                      </td>
+
+                      <td className="py-3 px-6 text-left border">
+                        <button
+                          className="py-3 px-6"
+                          onClick={() => handleOnDelete(galleryItem._id)}
+                        >
+                          <MdDelete className="w-[25px] h-[25px] text-red-400 cursor-pointer" />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        ) : (
+          <div className="absolute top-0 left-0 w-full h-full z-50 flex justify-center items-center ">
+            <p className="w-[100px] h-[100px] border border-primary border-t-4 border-t-blue-700 rounded-full animate-spin"></p>
+          </div>
+        )}
       </div>
     </>
   );

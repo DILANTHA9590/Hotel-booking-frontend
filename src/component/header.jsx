2efc931791e108c619489c9decf7c8bca2f83cb4
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-04-06 15:43:36 +0530

    create booking room using header  input filed✅📄

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 import Categories from "./categories";
 import Services from "./services";
+import axios from "axios";
+import toast from "react-hot-toast";
+import Footer from "./footer";
 
 export default function Header() {
+  const [startdate, setStartDate] = useState();
+  const [enddate, setEndtDate] = useState();
+  const [category, setCategory] = useState();
+
   const images = [
     "https://images.pexels.com/photos/2403209/pexels-photo-2403209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     "https://images.pexels.com/photos/2739666/pexels-photo-2739666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
@@ -22,6 +29,40 @@ export default function Header() {
     return () => clearInterval(interval); // Cleanup
   }, [images]);
 
+  function onClickBooking() {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      return toast.error("Please Login first");
+    }
+
+    if (!startdate || !enddate || !category) {
+      return toast.error("All input filed required");
+    }
+    const bookingData = {
+      start: startdate,
+      end: enddate,
+      category: category.toLowerCase(), // Convert category to lowercase
+    };
+
+    axios
+      .post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/booking/createbycategory`,
+        bookingData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <>
       <div
@@ -35,14 +76,26 @@ export default function Header() {
         <div className="text-white font-bolds flex gap-y-10  sm:gap-x-10 text-2xl items-center  p-5 flex-col sm:flex-row sm:transform  rounded-bl-2xl rounded-tr-2xl border-2">
           <div className="flex flex-col">
             <label htmlFor="">Check In</label>
-            <input type="date" className="border border-primary b- w-64" />
+            <input
+              type="date"
+              className="border border-primary b- w-64"
+              onChange={(e) => setStartDate(e.target.value)}
+            />
           </div>
           <div className="flex flex-col">
             <label htmlFor="">Check In</label>
-            <input type="date" className="border border-primary w-64" />
+            <input
+              type="date"
+              className="border border-primary w-64"
+              onChange={(e) => setEndtDate(e.target.value)}
+            />
           </div>
           <div>
-            <select name="category" id="category">
+            <select
+              name="category"
+              id="category"
+              onChange={(e) => setCategory(e.target.value)}
+            >
               <option value="" className="text-black">
                 Select Category
               </option>
@@ -59,7 +112,10 @@ export default function Header() {
           </div>
 
           <div>
-            <button className="bg-secondry p-4 rounded-[130px] ">
+            <button
+              className="bg-secondry p-4 rounded-[130px] "
+              onClick={onClickBooking}
+            >
               Book Now
             </button>
           </div>
@@ -71,6 +127,10 @@ export default function Header() {
 
       <section>
         <Services />
+      </section>
+
+      <section>
+        <Footer />
       </section>
     </>
   );

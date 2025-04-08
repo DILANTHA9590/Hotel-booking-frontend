9bc6a82f02cfc1dce12d7e0fc50c979df5fbacb8
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-04-08 22:30:55 +0530

    add route for  contact us page✅🔗

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -14,6 +14,7 @@ import BookingForm from "../component/bookingForm";
 import About from "../component/about";
 import Rooms from "../component/admin/admin-panel-components/rooms";
 import AllRooms from "../component/admin/admin-panel-components/rooms";
+import ContactUs from "../component/contact";
 
 export default function HomePage() {
   const images = [
@@ -57,6 +58,7 @@ export default function HomePage() {
             <Route path="/booking" element={<BookingForm />} />
             <Route path="/about" element={<About />} />
             <Route path="/rooms" element={<AllRooms />} />
+            <Route path="/contact" element={<ContactUs />} />
           </Routes>
         </div>
       </div>

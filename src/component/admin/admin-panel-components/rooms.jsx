a6a0ffca8d2166abd45f38765cc3268cd7bb0946
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-04-08 22:32:01 +0530

    add category names for rooms component🔗✅

--- src/component/admin/admin-panel-components/rooms.jsx
+++ src/component/admin/admin-panel-components/rooms.jsx
@@ -5,16 +5,26 @@ import StandardRoom from "../../standardRoom";
 export default function AllRooms() {
   return (
     <>
-      <section>
-        <StandardRoom />
-      </section>
-
-      <section>
-        <DuluxRoom />
-      </section>
-      <section>
-        <LuxuryRoom />
-      </section>
+      <div>
+        <section className="">
+          <h1 className="text-7xl bg-secondry text-center font-bold">
+            Standard Rooms
+          </h1>
+          <StandardRoom />
+        </section>
+        <section>
+          <h1 className="text-7xl bg-secondry text-center font-bold">
+            Duluxe Rooms
+          </h1>
+          <DuluxRoom />
+        </section>
+        <section>
+          <h1 className="text-7xl bg-secondry text-center font-bold">
+            Luxury Rooms
+          </h1>
+          <LuxuryRoom />
+        </section>
+      </div>
     </>
   );
 }

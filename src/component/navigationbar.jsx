2f020953df1c17f2297ff21848a9eb9e7e2df5f6
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-04-11 20:11:06 +0530

    Moved navigation links to separate component and integrated into main navbar✅🌟

--- src/component/navigationbar.jsx
+++ src/component/navigationbar.jsx
@@ -2,37 +2,16 @@ import { Link } from "react-router";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
+import MobileNavigationBar from "./mobnav";
 export default function Navigationbar() {
   const [mobNavbarOpen, setMobNavbarOpen] = useState(false);
+
+  function navBarClose() {
+    setMobNavbarOpen(false);
+  }
   return (
     <>
-      {mobNavbarOpen && (
-        <div className="fixed w-screen h-screen bg-black/40 z-1">
-          <div className="w-[60%] h-full relative  backdrop-blur-xl">
-            <div>
-              <img
-                src="logo.webp"
-                alt=""
-                className="transform skew-x-12 rounded-bl-2xl rounded-tr-2xl w-[60px] h-[4 0px]"
-              />
-            </div>
-            <div
-              className="absolute top-0 right-0 p-1 "
-              onClick={() => setMobNavbarOpen(false)}
-            >
-              <AiOutlineClose className="w-[30px] h-[30px]" />
-            </div>
-
-            <div className=" mt-[100px-] ">
-              <p>test1</p>
-              <p>test</p>
-              <p>test</p>
-              <p>test</p>
-              <p>test</p>
-            </div>
-          </div>
-        </div>
-      )}
+      {mobNavbarOpen && <MobileNavigationBar navbarclose={navBarClose} />}
 
       <div className="flex justify-between p-6 backdrop-blur-sm   text-2xl font-bold items-center h-[15vh] text-primary">
         <div>

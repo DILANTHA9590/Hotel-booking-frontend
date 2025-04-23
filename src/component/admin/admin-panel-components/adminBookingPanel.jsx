4a5d087431ea76dd7ad6965412b19bf1caab1503
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-04-23 22:56:02 +0530

    Applied props destructuring for cleaner and readable component(adminbookingpanel)✅🌟

--- src/component/admin/admin-panel-components/adminBookingPanel.jsx
+++ src/component/admin/admin-panel-components/adminBookingPanel.jsx
@@ -1,20 +1,29 @@
 import { MdDelete } from "react-icons/md";
-export default function AdminBookingPanel(props) {
-  const booking = props;
+export default function AdminBookingPanel({
+  bookingId,
+  roomId,
+  email,
+  status,
+  handleStatusChange,
+  reason,
+  start,
+  end,
+  notes,
+  timeStamps,
+  deleteBooking,
+}) {
   return (
     <>
-      <td className="py-3 px-6 text-left border">{booking.bookingId}</td>
-      <td className="py-3 px-6 text-left border">{booking.roomId}</td>
-      <td className="py-3 px-6 text-left border">{booking.email}</td>
+      <td className="py-3 px-6 text-left border">{bookingId}</td>
+      <td className="py-3 px-6 text-left border">{roomId}</td>
+      <td className="py-3 px-6 text-left border">{email}</td>
       <td className="py-3 px-6 text-left border">
         <select
           name="status"
           id="status"
           className="w-[80px]"
-          value={booking.status}
-          onChange={(e) =>
-            booking.handleStatusChange(booking.bookingId, e.target.value)
-          }
+          value={status}
+          onChange={(e) => handleStatusChange(bookingId, e.target.value)}
         >
           <option value="">Status</option>
           <option value="Confirm">Confirmed</option>
@@ -23,16 +32,16 @@ export default function AdminBookingPanel(props) {
         </select>
       </td>
 
-      <td className="py-3 px-6 text-left border">{booking.reason}</td>
-      <td className="py-3 px-6 text-left border">{booking.start}</td>
-      <td className="py-3 px-6 text-left border">{booking.end}</td>
-      <td className="py-3 px-6 text-left border">{booking.notes}</td>
-      <td className="py-3 px-6 text-left border">{booking.timeStamps}</td>
+      <td className="py-3 px-6 text-left border">{reason}</td>
+      <td className="py-3 px-6 text-left border">{start}</td>
+      <td className="py-3 px-6 text-left border">{end}</td>
+      <td className="py-3 px-6 text-left border">{notes}</td>
+      <td className="py-3 px-6 text-left border">{timeStamps}</td>
       <td className="py-3 px-6 text-left border">
         <button className="py-3 px-6 ">
           <MdDelete
             className=" text-center text-red-500 text-2xl"
-            onClick={() => booking.deleteBooking(booking.bookingId)}
+            onClick={() => deleteBooking(bookingId)}
           />
         </button>
       </td>
